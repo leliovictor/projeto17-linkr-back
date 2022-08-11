19a5a1d5cef44112ac102690c520de0a23428f13
lucasbrandao <brandslucas@gmail.com>
2022-08-11 18:17:10 -0300

    popula tabela hashtags e hashtagPosts

--- src/controllers/newPostController.js
+++ src/controllers/newPostController.js
@@ -1,11 +1,33 @@
-import  addNewPost  from "../repositories/newPostRepository.js";
+import { newPostRepository } from "../repositories/newPostRepository.js";
 
 export async function newPost(req, res){
     const  newPost  = res.locals.body;
     const  data  = res.locals.data;
 
+    function findHashtags(str){
+        const hashtags = str.match(/#\w+/g);
+          if(hashtags){
+            return hashtags.map(hashtag => hashtag.slice(1));
+          }
+          return [];
+    }
+
     try{
-        await addNewPost(newPost.url, newPost.message, data.id);
+        await newPostRepository.addNewPost(newPost.url, newPost.message, data.id);
+        if (newPost.message){
+            const hashtags = findHashtags(newPost.message)
+            if(hashtags.length > 0){
+                for (let i=0; i<hashtags.length; i++){
+                    const registeredHashtags = await newPostRepository.getHashtagsByName(hashtags[i]);
+                    if (!registeredHashtags.rows[0]){
+                        await newPostRepository.addNewHashtag(hashtags[i]);                        
+                    }
+                    const postId = await newPostRepository.getPosts(newPost.message, data.id);
+                    const hashtagId = await newPostRepository.getHashtagsByName(hashtags[i]);
+                    await newPostRepository.addNewHashtagPost(newPost.url, hashtagId.rows[0].id, postId.rows[0].id);    
+                }
+            }
+        }
         res.sendStatus(201);
     }catch (error){
         res.sendStatus(error)
--- src/repositories/newPostRepository.js
+++ src/repositories/newPostRepository.js
@@ -1,9 +1,52 @@
 import connection from "../config/db.js";
 
-export default async function addNewPost (url, message, userId){
+async function getHashtags(){
+    const query = `SELECT * FROM hashtags`
+
+    return connection.query(query)
+
+}
+
+async function getHashtagsByName(hashtag){
+    const query = `SELECT * FROM hashtags WHERE hashtag=$1`
+
+    const value = [hashtag]
+
+    return connection.query(query,value)
+
+}
+
+async function getPosts(message, userId){
+    const query = `SELECT * FROM posts WHERE (message=$1 AND "userId" = $2)`
+
+    const value = [message, userId]
+
+    return connection.query(query, value)
+}
+
+async function addNewPost (url, message, userId){
     const query = `INSERT INTO posts (url, message, "userId") VALUES ($1, $2, $3)`;
 
     const values = [url, message, userId];
 
     return connection.query(query,values);
 }
+
+async function addNewHashtag(hashtag){
+    const query = `INSERT INTO hashtags (hashtag) VALUES ($1)`;
+
+    const value = [hashtag];
+
+    return connection.query(query,value);
+}
+
+async function addNewHashtagPost(url, hashtagId, postId){
+
+    const query = `INSERT INTO "hashtagsPosts" (url, "hashtagId", "postId") VALUES ($1, $2, $3)`;
+
+    const values = [url, hashtagId, postId];
+
+    return connection.query(query,values);
+}
+
+export const newPostRepository =  {getHashtags, getHashtagsByName, getPosts, addNewPost, addNewHashtag, addNewHashtagPost}

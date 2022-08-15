4343e4bdbcc2cd91aef7483777050f21e279f34a
J040V1T0R <joaovitor.jovg73@gmail.com>
2022-08-15 09:48:32 -0300

    version 1.1 09:49

--- src/config/db.js
+++ src/config/db.js
@@ -4,18 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'brandao92167';
-const host = 'localhost';
-const port = 5432;
-const database = 'linkr_database';
-
 const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
 });
 
 export default connection;
--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -3,14 +3,12 @@ import timelineRepository from "../repositories/timelineRepository.js";
 async function getTrendingList(req,res){
 	
 	try{
-		const trendingList = await timelineRepository.getTredingHashtags()
+		const { rows: trendingList } = await timelineRepository.getTredingHashtags()
 		
-		res.status(200).send(trendingList.rows)
-		
-	  }catch (error){
-		res.sendStatus(error)
-	  }    
-	
+		res.status(200).send(trendingList)
+	} catch (error) {
+		res.sendStatus(500)
 	};
+};
 
 export { getTrendingList };
